bf6f6064fc09699f0d4c54d30a11d99be6a0f3f6
Brandon Thomson <brandon.j.thomson@gmail.com>
2010-12-06 02:04:26 -0500

    aws project improvements

--- script.py
+++ script.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python
 # NFS EC2 Test Script
 # By Brandon Thomson
-# 
+
+# Requires Python 2.7 or newer
 # Mostly designed to be used at the interactive iPython prompt
-# 
+#
 from __future__ import print_function
 
 from boto.ec2.connection import EC2Connection
 import time
 import subprocess
+import logging, logging.config
+import yaml
+
+with open('log_config.yaml') as f:
+  config_dict = yaml.load(f.read())
+
+logging.config.dictConfig(config_dict)
 
 #AMAZON_LINUX_EBS = "ami-2272864b" # Required for micro instances
 
 # 099720109477/ebs/ubuntu-images/ubuntu-lucid-10.04-amd64-server-20101020
 UBUNTU_IMAGE = "ami-4a0df923"
-
-DEBUG_MODE = False
 
 # No upgrade here; we don't care about security
 #
@@ -28,8 +34,7 @@ with open("passwords.txt") as f:
   key_id, key_secret, _ = f.read().split("\n")
 
 conn = EC2Connection(key_id, key_secret)
-if DEBUG_MODE:
-  print("EC2 Connection opened")
+logging.debug("New EC2 Connection opened")
 
 def get_active_instances():
   instances = []
@@ -37,13 +42,11 @@ def get_active_instances():
   for rsv in conn.get_all_instances():
     instances += rsv.instances
 
-  if DEBUG_MODE:
-    print("{0} total instances found.".format(len(instances)))
+  logging.debug("{0} total instances found.".format(len(instances)))
 
   instances = [inst for inst in instances if inst.state == u'running']
 
-  if DEBUG_MODE:
-    print("{0} active instances found.".format(len(instances)))
+  logging.debug("{0} active instances found.".format(len(instances)))
 
   return instances
 
@@ -68,15 +71,15 @@ def wait_all_active(instances):
     try:
       status = [inst.update() for inst in instances]
       if all(x == u'running' for x in status):
-        print("All instances ready.")
+        logging.info("All instances ready.")
         return
       else:
-        print("{0}/{1} instances ready.".format(status.count(u'running'),
+        logging.info("{0}/{1} instances ready.".format(status.count(u'running'),
                                                 len(instances)))
 
     except boto.EC2ResponseError:
-      print("EC2ResponseError")
-    print("Will recheck in {0} seconds.".format(CHECK_DELAY))
+      logging.error("EC2ResponseError")
+    logging.info("Will recheck in {0} seconds.".format(CHECK_DELAY))
     time.sleep(CHECK_DELAY)
 
 ssh_args = [
@@ -87,7 +90,7 @@ ssh_args = [
 
 def start_n_micro(n):
   if not 0 < n < 4: # Safety valve
-    print("Warning: {0} is too many instances.".format(n))
+    logging.error("Warning: {0} is too many instances.".format(n))
     return
   resv = conn.run_instances(
     instance_type="t1.micro",
@@ -110,41 +113,40 @@ def get_ssh_cmd_line(n):
 def ssh_cmd(instance, args, block=False):
   host_str = 'ubuntu@' + instance.public_dns_name
   f_args = ssh_args + [host_str] + args
-  if DEBUG_MODE:
-    print(" ".join(f_args))
+  logging.debug(" ".join(f_args))
   if block:
     subprocess.check_call(f_args)
   else:
     return subprocess.Popen(f_args, stdout=subprocess.PIPE)
 
 def mount_nfs_share(client, server):
-  print("mounting nfs share on client")
+  logging.debug("mounting nfs share on client")
   ssh_cmd(client, ['sudo', 'mount',
                    server.private_ip_address + ":/tmp/ramdisk",
                    "/mnt/remote_ramdisk_1"], block=True)
 
 def unmount_nfs_share(client, server):
-  print("unmounting nfs share on client")
+  logging.debug("unmounting nfs share on client")
   ssh_cmd(client, ['sudo', 'umount', '-l', '/mnt/remote_ramdisk_1'], block=True)
 
 def unmount_ramdisk(machine):
-  print("unmounting disk on server to clear any cache")
+  logging.debug("unmounting disk on server to clear any cache")
   umount_cmd = 'sudo umount -l /tmp/ramdisk'
   ssh_cmd(machine, umount_cmd.split(" "), block=True)
 
 def mount_ramdisk(machine):
-  print("remounting fresh ramdisk on server")
+  logging.debug("remounting fresh ramdisk on server")
   mount_cmd = "sudo mount -t tmpfs -o size=384M tmpfs /tmp/ramdisk"
   ssh_cmd(machine, mount_cmd.split(" "), block=True)
 
 def nfs(client_id, server_id):
   client, server = id_to_inst(client_id, server_id)
-  print("nfs transfer from {0} -> {1}".format(client_id, server_id))
+  logging.debug("nfs transfer from {0} -> {1}".format(client_id, server_id))
 
   mount_nfs_share(client, server)
 
   try:
-    print("Transferring file")
+    logging.debug("Transferring file")
     dd_cmd = "dd if=/dev/random of=/mnt/remote_ramdisk_1/test bs=1M count=1"
     ssh_cmd(client, dd_cmd.split(" "), block=True)
   finally:
@@ -155,23 +157,24 @@ def nfs(client_id, server_id):
 
 def id_to_inst(*args):
   """Turns id numbers into instances"""
-  return (instances[x] for x in args)
+  if type(args[0]) == type(0):
+    return (instances[x] for x in args)
+  return args
 
 def iperf_2(id1, id2):
   i1, i2 = id_to_inst(id1, id2)
-  print("Iperf from {0} -> {1}".format(id1, id2))
+  logging.info("Iperf from {0} -> {1}".format(id1, id2))
   p1 = ssh_cmd(i1, ['iperf', '-c', i2.private_ip_address])
 
   stdout, _ = p1.communicate();
-  if DEBUG_MODE:
-    print(stdout)
+  logging.debug("Raw output from iperf:\n" + stdout)
   return stdout.split('\n')[6].split("    ")
 
-def iperf_all():
+def iperf_all_sequential():
   for i1 in instances:
     for i2 in instances:
       if i1 != i2:
-        interval, size, speed = iperf2(i1, i2)
+        interval, size, speed = iperf_2(i1, i2)
 
 def term_all():
   for i in instances:
--- setup_script.sh
+++ setup_script.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 # Instance setup script (runs before instance reported as ready)
-
-# Install required software. Every instance gets the same thing. Wastes
+#
+# Installs required software. Every instance gets the same thing. Wastes
 # bandwidth relative to setting this up pre-deployment, but who cares.
-# No upgrade needed; we're not really worred about security
+#
+# Note: this runs as root, so no need to mess with sudo
+#
 PACKAGES="iperf nfs-kernel-server nfs-common portmap"
 
-sudo apt-get -y update
-sudo apt-get -y install $PACKAGES
+apt-get -y update
+apt-get -y install $PACKAGES
+# No upgrades needed; we're not really worred about security
 
 # Start iperf server
 iperf -s &
 
 # Create and mount ramdisk
 mkdir /tmp/ramdisk; chmod 777 /tmp/ramdisk
-sudo mount -t tmpfs -o size=384M tmpfs /tmp/ramdisk/
+mount -t tmpfs -o size=384M tmpfs /tmp/ramdisk/
 
 # Create mount points for remote ramdisks
 mkdir /mnt/remote_ramdisk_1
 
 # Setup NFS server. No security since it is already setup at the AWS layer
 EXPORT_STR="/tmp/ramdisk 10.0.0.0/8(rw,insecure,no_subtree_check,async,fsid=0)"
-sudo -s
 echo $EXPORT_STR >> /etc/exports
 service nfs-kernel-server restart
